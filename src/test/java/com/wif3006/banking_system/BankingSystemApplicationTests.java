0fcb6dcad5218114da85e4703c619064f20d98e2
teojiesern <teojiesern42@gmail.com>
2026-01-13 22:40:57 +0800

    feat(app): init project

---
+++ src/test/java/com/wif3006/banking_system/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wif3006.banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
